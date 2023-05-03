649fa5150d5b59a11850ae6058e896aff176e9ec
yukiko0902 <yukikotakeuchi0902@gmail.com>
2023-05-04 00:04:25 +0900

    ユーザー管理機能（訂正４）

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,8 @@ class User < ApplicationRecord
   validates :nickname, presence: true
   validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
   validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
-  validates :last_name_kanacharacters, presence: true, format: { with: /\A([ァ-ン]|ー)+\z/}
-  validates :first_name_kanacharacters, presence: true, format: { with: /\A([ァ-ン]|ー)+\z/}
+  validates :last_name_kanacharacters, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
+  validates :first_name_kanacharacters, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
   validates :birthday, presence: true
   
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
